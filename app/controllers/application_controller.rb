6700c9a513cc395ee9badfd18a49b28329a14601
Bernie Chiu <bernie@shoplineapp.com>
2017-03-27 19:00:21 +0800

    Move reponse to global method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def error(message)
+    return render status: :error, json: { message: message }
+  end
+
+  def ok(data = {})
+    return render status: status, json: data
+  end
 end

09ac7a13e34b5b40d784fb2b57a98d86ebe131e7
Bernie Chiu <bernie@shoplineapp.com>
2017-03-28 18:44:24 +0800

    Support different format

--- app/controllers/v1/images_controller.rb
+++ app/controllers/v1/images_controller.rb
@@ -1,14 +1,15 @@
 class V1::ImagesController < ApplicationController
   def resize
     image_id = params[:image_id].to_s
+    format = params[:format].to_s
     height = params[:height].to_i
     width  = params[:width].to_i
     error("Required parameters not met!") unless V1::ImageService.validate_resize_params(image_id, height, width)
 
-    image = Dragonfly.app.fetch_url(V1::ImageService.parse_img_url(image_id))
+    image = Dragonfly.app.fetch_url(V1::ImageService.parse_img_url(image_id, format))
     height = nil if height == 0
     width  = nil if width == 0
-    resized_image = image.resize("#{width}x#{height}")
+    resized_image = image.resize("#{width}x#{height}", format: image.format)
     send_data resized_image, type: resized_image.mime_type, disposition: 'inline' if stale?(resized_image)
   end
 end
--- app/services/v1/image_service.rb
+++ app/services/v1/image_service.rb
@@ -1,8 +1,11 @@
 class V1::ImageService
-  RESIZE_LIMIT = 50
+  RESIZE_LIMIT = 50.freeze
+  SUPPORTED_FORMATS = ['jpg', 'png'].freeze
 
-  def self.parse_img_url(id)
-    "https://img.shoplineapp.com/media/image_clips/#{id}/original.jpg"
+  def self.parse_img_url(id, format)
+    format = format if SUPPORTED_FORMATS.include?(format)
+    format = 'jpg'  if format.blank?
+    "https://img.shoplineapp.com/media/image_clips/#{id}/original.#{format}"
   end
 
   def self.validate_resize_params(id, height, width)
